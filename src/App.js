3b1542b09b72481efedf5bcd23e21c6852b26f87
Laxmi Prasanna <laxmiprasanna59@gmail.com>
2023-07-04 19:46:58 +0530

    created a simple homepage route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Homepage from "./pages/Homepage";
 function App() {
-  return <div></div>;
+  // 1. create router
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Homepage />
+    }
+  ]);
+  // 2. provide router
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/Homepage.js
@@ -0,0 +1,5 @@
+const Homepage = () => {
+	return <h1>My Homepage</h1>
+}
+
+export default Homepage;
