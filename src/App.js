405bb7140a0fac2135450000b43b6f317f691583
Laxmi Prasanna <laxmiprasanna59@gmail.com>
2023-07-05 07:46:33 +0530

    create another route - products

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Homepage from "./pages/Homepage";
+import Products from "./pages/Products";
 function App() {
   // 1. create router
   const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Homepage />
-    }
+    { path: "/", element: <Homepage /> },
+    { path: "/products", element: <Products /> }
   ]);
   // 2. provide router
   return <RouterProvider router={router} />;
