0ed82e963722aa0602702cc581c2bd34dfc978d2
Laxmi Prasanna <laxmiprasanna59@gmail.com>
2023-07-05 11:17:03 +0530

    Link to other routes

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,5 +1,12 @@
+import { Fragment } from "react";
+import { Link } from "react-router-dom";
 const Homepage = () => {
-	return <h1>My Homepage</h1>
+	return (
+		<>
+			<h1>My Homepage</h1>
+			<Link to="/products">Products</Link>
+		</>
+	)
 }
 
 export default Homepage;
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,12 @@
+import { Fragment } from "react";
+import { Link } from "react-router-dom";
 const Products = () => {
-	return <h1>The Products page</h1>
+	return (
+		<>
+			<h1>The Products page</h1>
+			<Link to="/">Back to homepage</Link>
+		</>
+	)
 }
 
 export default Products;
