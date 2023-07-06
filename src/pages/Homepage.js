e126e9ce3281964c3cfacb5f8669c716c850ea41
Laxmi Prasanna <laxmiprasanna59@gmail.com>
2023-07-06 15:06:45 +0530

    navigate on event/programatically u: useNavigate

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,8 +1,14 @@
 import { Fragment } from "react";
+import { useNavigate } from "react-router-dom";
 const Homepage = () => {
+	const navigate = useNavigate();
+	const productNavigationButtonHandler = () => {
+		navigate("/products");
+	}
 	return (
 		<>
 			<h1>My Homepage</h1>
+			<button onClick={productNavigationButtonHandler}>Navigate To Products</button>
 		</>
 	)
 }
